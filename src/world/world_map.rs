159f9754c95d312ed6cfd2e94c6fb124fde7be32
William Russell Miller <russmiller2@gmail.com>
2015-05-12 20:32:55 -0400

    feature_generators

--- src/world/world_map.rs
+++ src/world/world_map.rs
@@ -23,35 +23,18 @@ impl WorldMap {
         let mut world = WorldMap { width: width, height: height, tiles: tiles };
 
         // Generate random features.
-        // Tried to make it generate each room a random shape, 
-        // but instead it does all circles or all squares
-        let t = rng.gen_range::<i32>(3,13);
-        let mut feature_shapes: Vec<(Box<Fn(&mut R) -> FeatureBuilder>, u32) > =
-            vec![
-                (Box::new(|rng: &mut R|{
-                    FeatureBuilder::room(0,0)
-                }), 1)
-            ];
-        if t >= 8 {
-            feature_shapes= 
-                vec![
+        let mut feature_generators: Vec<(Box<Fn(&mut R) -> FeatureBuilder>, u32)            > = vec![
                     (Box::new(|rng: &mut R| {
                         let i = rng.gen_range::<i32>(3,15);
                         let j = rng.gen_range::<i32>(3,15);
                         FeatureBuilder::room(i,j)
-                    }), 1)
-                ];
-        }
-        else {
-            feature_shapes= 
-                vec![
+                    }), 1),
                     (Box::new(|rng: &mut R| {
                         let j = rng.gen_range::<i32>(3,15);
                         FeatureBuilder::room_circle(j)
                     }), 1)
                 ];
-        }
-        let feature_table = RandomTable::new(feature_shapes);
+        let feature_table = RandomTable::new(feature_generators);
         let mut features: Vec<Feature> = Vec::new();
         'outer: while features.len() < 12 {
             let feature_builder = feature_table.generate(rng);
@@ -383,9 +366,8 @@ impl FeatureBuilder {
 
         FeatureBuilder::new(components)
     }
-    pub fn room_circle(height: i32) -> Self {
+    pub fn room_circle(radius: i32) -> Self {
         let mut components = Vec::new();
-        let radius = height / 2;
         for x in -radius..radius+1 {
             for y in -radius..radius+1 {
                 let terrain = 
@@ -407,7 +389,7 @@ impl FeatureBuilder {
     pub fn distance_from_center(xi: i32,yi: i32) -> i32 {
         let c: i32 = (xi * xi) + (yi * yi);
         let c = (c as f64).sqrt();
-    return c as i32;
+        c as i32
     }
     pub fn location(mut self, loc: Location) -> Self {
         self.location = loc;
